98059616c6b9bd8b11c82b8e3dd9888a1da12211
Dongyu Gong <dongyu.gong@new.ox.ac.uk>
2026-09-04 19:52:46 -0400

    refactor: remove "Delete account" link from footer across multiple pages for improved navigation consistency

--- scripts/build-blog.js
+++ scripts/build-blog.js
@@ -247,7 +247,6 @@ const FOOTER = `
                     <a href="/contact">Contact</a>
                     <a href="/privacy">Privacy</a>
                     <a href="/terms">Terms</a>
-                    <a href="/delete-account">Delete account</a>
                 </div>
             </div>
             <div class="footer-bottom">
